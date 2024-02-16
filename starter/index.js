9fa743a7be6db43ae0354ebcd4161327af945f5e
LeonieWhitehead <leoniewhitehead@hotmail.co.uk>
2024-02-16 19:12:49 +0000

    added prompt questions and a validation function for project name

--- starter/index.js
+++ starter/index.js
@@ -5,7 +5,60 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter a title for your project:',
+    validate: titleInput => {
+        if (titleInput) {
+            return true;
+        } else {
+            console.log('Please enter a title for your project.');
+            return false;
+        }
+    }
+},
+{
+  type: 'input',
+  name: 'description',
+  message: 'Provide a description of your project:'
+},
+{
+  type: 'input',
+  name: 'installation',
+  message: 'Provide installation instructions:'
+},
+{
+  type: 'input',
+  name: 'usage',
+  message: 'Provide usage information:'
+},
+{
+  type: 'list',
+  name: 'license',
+  message: 'Choose a license for your application:',
+  choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+},
+{
+  type: 'input',
+  name: 'contributing',
+  message: 'Provide contribution guidelines:'
+},
+{
+  type: 'input',
+  name: 'tests',
+  message: 'Provide test instructions:'
+},
+{
+  type: 'input',
+  name: 'githubUsername',
+  message: 'Enter your GitHub username:'
+},
+{
+  type: 'input',
+  name: 'email',
+  message: 'Enter your email address:'
+}
 ];
 
 // function to write README file
